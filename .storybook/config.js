c8241d8d48998ffe13f97c33cc6d76deb6c482bb
Martin Danielson <martin.danielsson@schibsted.se>
2017-04-05 10:04:10 +0200

    Updates

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@kadira/storybook';
-
-const req = require.context('../packages/', true, /stories\/index.js$/)
-
-function loadStories() {
-    req.keys().forEach((filename) => req(filename))
-}
-
-configure(loadStories, module);
